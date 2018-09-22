9dd28841311aad10998e8c067944b4bbb959a85b
Stephen Burnett <steve@shareserv.net>
2018-09-22 18:26:43 +0100

    Initial creation

--- fd-api-cli/Module1.vb
+++ fd-api-cli/Module1.vb
@@ -1,4 +1,10 @@
 ﻿Module Module1
+    Private Const str_bannerText As String =
+        "************************************************************" & vbCrLf &
+        "**  FD-API-CLI - A command line tool to access the        **" & vbCrLf &
+        "**               Freshdesk API                            **" & vbCrLf &
+        "************************************************************" & vbCrLf
+
 
     Sub Main()
 
